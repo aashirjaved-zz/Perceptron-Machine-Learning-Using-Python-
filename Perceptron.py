2b273989e6457570c7b97448b3c9630431e677e5
Aashir Javed <aas.jav@gmail.com>
2017-05-31 22:00:25 +0500

    Dataset link updated

--- Perceptron.py
+++ Perceptron.py
@@ -37,7 +37,7 @@ class Perceptron(object):
 # as its binary classifier
 # considering two classes
 
-df = pd.read_csv('dataset.csv', header=None)
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
 y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', -1, 1)
 X = df.iloc[0:100, [0, 2]].values
